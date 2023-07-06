623349a5c0b1a8a71b6dbfe863db1d74c57b8adf
Jayke770 <jakevelasco40@gmail.com>
2023-07-06 08:18:01 +0800

    .

---
+++ components/background-effect.tsx
@@ -0,0 +1,41 @@
+import { motion } from 'framer-motion'
+import Image from 'next/image'
+import BombLogo from "@/public/assets/images/bomb.png"
+import TileLogo from "@/public/assets/images/tile.png"
+const icons = [BombLogo, TileLogo]
+const particles = Array(Math.floor((Math.random() * 50)) + 20).fill(0)
+export default function Background() {
+    return (
+        <>
+            {particles.map((_, i) => (
+                <motion.div
+                    key={i}
+                    className='absolute'
+                    initial={{ y: 0, x: 0 }}
+                    animate={{ y: -Math.random() * 5, x: -Math.random() * 5 }}
+                    exit={{ y: 0, x: 0 }}
+                    transition={{
+                        delay: i * 0.1,
+                        duration: 0.5,
+                        repeatDelay: Math.random() * 2,
+                        repeat: Infinity,
+                        repeatType: 'reverse',
+                        ease: 'easeInOut',
+                    }}
+                    style={{
+                        right: `${Math.random() * 100}vw`,
+                        top: `${Math.random() * 100}vh`,
+                        left: `${Math.random() * 100}vw`,
+                        rotate: Math.random() * 360
+                    }}>
+                    <Image
+                        src={icons[Math.floor((Math.random() * 2))]}
+                        priority
+                        alt="icon"
+                        className='h-12 w-12 object-contain' />
+                </motion.div >
+            ))
+            }
+        </>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import Image from 'next/image'
 import BombLogo from "@/public/assets/images/bomb.png"
 import TileLogo from "@/public/assets/images/tile.png"
 import Head from 'next/head'
+import Background from '@/components/background-effect'
 const tile = new Howl({ src: ["/assets/sounds/tile.mp3"], volume: 0.7, preload: true })
 const bomb = new Howl({ src: ["/assets/sounds/bomb.mp3"], volume: 0.7, preload: true })
 export default function Game() {
@@ -34,52 +35,56 @@ export default function Game() {
       <Head>
         <title>Mines</title>
       </Head>
+
       <motion.main
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
-        className="h-screen w-screen flex justify-center items-center p-4">
-        <div className="w-full md:w-[30rem] dark:bg-secondary-dark p-4 shadow-xl rounded-lg">
-          <div className="flex justify-center gap-2 items-center py-2 text-4xl ">
-            {emoji("💣")}
-            <h1 className="font-bold">Mines</h1>
-          </div>
-          <div className="grid grid-cols-5 gap-2 mt-5">
-            {Array(25).fill(0).map((_, i) => (
-              <motion.div
-                key={i}
-                whileHover={{ scale: 0.9 }}
-                whileTap={{ scale: 1.1 }}
-                onClick={() => onCheckBlock(i)}
-                className={`${gameData?.isGameOver || clickBlocks.includes(i) ? gameData?.mines.includes(i) ? "tile-bomb" : "tile-star" : "tile-active"} transition-all`}>
-                {gameData?.isGameOver || clickBlocks.includes(i) ? (
-                  <motion.div
-                    key={`block-${i}`}
-                    initial={{ scale: 0.6 }}
-                    animate={{ scale: 1 }}
-                    exit={{ scale: 0.6 }}
-                    transition={{ type: "spring", bounce: 0.7, duration: 0.8 }}
-                    className="p-0.5 md:p-2">
-                    <Image
-                      src={gameData?.mines.includes(i) ? BombLogo : TileLogo}
-                      priority
-                      alt={gameData?.mines.includes(i) ? "bomd" : "tile"}
-                      className='object-contain w-full h-full' />
-                  </motion.div>
-                ) : null}
-              </motion.div>
-            ))}
-          </div>
-          <div className='flex flex-col mt-5 px-4'>
-            <motion.button
-              onClick={onRestart}
-              disabled={!gameData?.isGameOver}
-              className="h-12 disabled:dark:bg-amber-600/10 disabled:cursor-not-allowed disabled:dark:text-zinc-500 w-full rounded-xl p-1 text-base font-semibold shadow transition-all dark:bg-amber-600 dark:focus:ring-2 dark:focus:ring-amber-600 dark:focus:ring-offset-1 dark:focus:ring-offset-secondary-dark">
-              Restart
-            </motion.button>
+        className="h-screen w-screen flex justify-center items-center p-4 relative overflow-hidden">
+        <Background />
+        <div className='absolute w-full h-full flex justify-center items-center p-4'>
+          <div className="w-full md:w-[30rem] dark:bg-secondary-dark translucent shadow-xl p-4 rounded-lg">
+            <div className="flex justify-center gap-2 items-center py-2 text-4xl ">
+              {emoji("💣")}
+              <h1 className="font-bold">Mines</h1>
+            </div>
+            <div className="grid grid-cols-5 gap-2 mt-5">
+              {Array(25).fill(0).map((_, i) => (
+                <motion.div
+                  key={i}
+                  whileHover={{ scale: 0.9 }}
+                  whileTap={{ scale: 1.1 }}
+                  onClick={() => onCheckBlock(i)}
+                  className={`${gameData?.isGameOver || clickBlocks.includes(i) ? gameData?.mines.includes(i) ? "tile-bomb" : "tile-star" : "tile-active"} transition-all`}>
+                  {gameData?.isGameOver || clickBlocks.includes(i) ? (
+                    <motion.div
+                      key={`block-${i}`}
+                      initial={{ scale: 0.6 }}
+                      animate={{ scale: 1 }}
+                      exit={{ scale: 0.6 }}
+                      transition={{ type: "spring", bounce: 0.7, duration: 0.8 }}
+                      className="p-0.5 md:p-2">
+                      <Image
+                        src={gameData?.mines.includes(i) ? BombLogo : TileLogo}
+                        priority
+                        alt={gameData?.mines.includes(i) ? "bomd" : "tile"}
+                        className='object-contain w-full h-full' />
+                    </motion.div>
+                  ) : null}
+                </motion.div>
+              ))}
+            </div>
+            <div className='flex flex-col mt-5 px-4'>
+              <motion.button
+                onClick={onRestart}
+                disabled={!gameData?.isGameOver}
+                className="h-12 disabled:dark:bg-amber-600/10 disabled:cursor-not-allowed disabled:dark:text-zinc-500 w-full rounded-xl p-1 text-base font-semibold shadow transition-all dark:bg-amber-600 dark:focus:ring-2 dark:focus:ring-amber-600 dark:focus:ring-offset-1 dark:focus:ring-offset-secondary-dark">
+                Restart
+              </motion.button>
+            </div>
           </div>
         </div>
-      </motion.main>
+      </motion.main >
     </>
   )
 }
